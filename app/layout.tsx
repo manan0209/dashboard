aa9ec47ba0ce24f1609bd88f23218e31f21f8776
Manan Goel <manangoel0209@gmail.com>
2025-01-26 09:38:25 +0530

    Analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,24 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import { Analytics } from "@vercel/analytics/react";
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Ghostsales Dashboard',
-  description: 'Ghostsales - The ultimate AI-powered CRM designed to automate and supercharge your sales process. Manage leads, engage customers, and drive growth like never before.',
-}
+  title: "Ghostsales Dashboard",
+  description:
+    "Ghostsales - The ultimate AI-powered CRM designed to automate and supercharge your sales process. Manage leads, engage customers, and drive growth like never before.",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
-  )
+  );
 }
